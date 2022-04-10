a5e78e5f6c82bdb3acafb42d78c1cfdec547aa8a
de-bratchikov <de-bratchikov@yandex.ru>
2022-04-10 16:12:06 +0500

    Theme 1 tasks 1-3 Ready (ed.2)

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,100 +4,36 @@ let deadMole = document.getElementById("dead");
 let misses = document.getElementById("lost")
 
 // getHole = index => document.getElementById(`hole${index}`)
-// for (let i = 0; i < 10; i++) {
-    // можно все элементы включить в массив, и для массива сделать обход)
-// }
+
 const h1 = document.getElementById(`hole1`)
-
-h1.onclick = () => {
-    if (h1.classList.contains( 'hole_has-mole' )) {
-        deadMole.textContent = Number(deadMole.textContent) + 1;
-    } else {
-        misses.textContent = Number(misses.textContent) + 1;
-    }
-}
-
 const h2 = document.getElementById(`hole2`)
-
-h2.onclick = () => {
-    if (h2.classList.contains( 'hole_has-mole' )) {
-        deadMole.textContent = Number(deadMole.textContent) + 1;
-    } else {
-        misses.textContent = Number(misses.textContent) + 1;
-    }
-}
-
 const h3 = document.getElementById(`hole3`)
-
-h3.onclick = () => {
-    if (h3.classList.contains( 'hole_has-mole' )) {
-        deadMole.textContent = Number(deadMole.textContent) + 1;
-    } else {
-        misses.textContent = Number(misses.textContent) + 1;
-    }
-}
-
 const h4 = document.getElementById(`hole4`)
-
-h4.onclick = () => {
-    if (h4.classList.contains( 'hole_has-mole' )) {
-        deadMole.textContent = Number(deadMole.textContent) + 1;
-    } else {
-        misses.textContent = Number(misses.textContent) + 1;
-    }
-}
-
 const h5 = document.getElementById(`hole5`)
-
-h5.onclick = () => {
-    if (h5.classList.contains( 'hole_has-mole' )) {
-        deadMole.textContent = Number(deadMole.textContent) + 1;
-    } else {
-        misses.textContent = Number(misses.textContent) + 1;
-    }
-}
-
 const h6 = document.getElementById(`hole6`)
-
-h6.onclick = () => {
-    if (h6.classList.contains( 'hole_has-mole' )) {
-        deadMole.textContent = Number(deadMole.textContent) + 1;
-    } else {
-        misses.textContent = Number(misses.textContent) + 1;
-    }
-}
-
 const h7 = document.getElementById(`hole7`)
-
-h7.onclick = () => {
-    if (h7.classList.contains( 'hole_has-mole' )) {
-        deadMole.textContent = Number(deadMole.textContent) + 1;
-    } else {
-        misses.textContent = Number(misses.textContent) + 1;
-    }
-}
-
 const h8 = document.getElementById(`hole8`)
-
-h8.onclick = () => {
-    if (h8.classList.contains( 'hole_has-mole' )) {
-        deadMole.textContent = Number(deadMole.textContent) + 1;
-    } else {
-        misses.textContent = Number(misses.textContent) + 1;
-    }
-}
-
 const h9 = document.getElementById(`hole9`)
 
-h9.onclick = () => {
-    if (h9.classList.contains( 'hole_has-mole' )) {
+let holeBox = [h1, h2, h3, h4, h5, h6, h7, h8, h9]
+
+// Декларативный подход через forEach
+holeBox.forEach(element => element.onclick = () => {
+    if (element.classList.contains( 'hole_has-mole' )) {
         deadMole.textContent = Number(deadMole.textContent) + 1;
     } else {
         misses.textContent = Number(misses.textContent) + 1;
     }
-}
-//     getHole.onclick = () => {
+    }
+)
 
+// Императивный подход через цикл for
+// for (let i = 0; i < 10; i++) {
+//     holeBox[i].onclick = () => {
+//         if (holeBox[i].classList.contains( 'hole_has-mole' )) {
+//             deadMole.textContent = Number(deadMole.textContent) + 1;
+//         } else {
+//             misses.textContent = Number(misses.textContent) + 1;
+//         }
 //     }
 // }
-
