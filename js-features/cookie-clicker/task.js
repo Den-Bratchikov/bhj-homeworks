e6cfee6e215ad32428448fd9dd55ee11ab51cac6
de-bratchikov <de-bratchikov@yandex.ru>
2022-04-30 17:47:12 +0500

    Theme 1.1 (js-features) tasks 1-3 Ready (ed.3)

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -8,27 +8,10 @@ let click1Time = 0;
 
 let click2Time = 0;
 
-let timeBox = []
+// let timeBox = []
 
 Cookie.onclick = () => {   
-    click1Time = new Date()
-    
-    let l = timeBox.length;
-
-    let timeDiff;
-
-    if (l <= 1) {
-        timeDiff = 0
-        timeBox.push(timeDiff)
-    } else {
-        timeDiff = (click1Time - click2Time) / 1000;
-        timeBox.push(timeDiff)
-    }
-
-    let sum = timeBox.reduce((acc, number) => acc + number, 0);
-    
-    clickSpeed.textContent = (sum / l).toFixed(2)
-    
+    //  ############ СЧЕТЧИК КЛИКОВ ############
     let n = Number(clickcount.textContent) + 1
     
     if (n % 2 !== 0) {
@@ -37,8 +20,36 @@ Cookie.onclick = () => {
         Cookie.width = 200
     }
 
-    clickcount.textContent = n
-
-    click2Time = click1Time;
+    clickcount.textContent = n;
     
-}
+    //  ############ СЧЕТЧИК СКОРОСТИ КЛИКОВ ############
+
+    click1Time = new Date()
+    
+    let timeDiff;
+
+    if(click2Time === 0) {
+        timeDiff = 0
+    } else {
+        timeDiff = (click1Time - click2Time) / 1000;
+    }
+    
+    clickSpeed.textContent = timeDiff.toFixed(2)
+    
+    click2Time = click1Time;
+    //  ############ СЧЕТЧИК СРЕДНЕЙ СКОРОСТИ КЛИКОВ ############
+    
+    // let l = timeBox.length;
+    
+    // if (l <= 1) {
+        //     timeDiff = 0
+        //     timeBox.push(timeDiff)
+        // } else {
+    //     timeDiff = (click1Time - click2Time) / 1000;
+    //     timeBox.push(timeDiff)
+    // }
+
+    // let sum = timeBox.reduce((acc, number) => acc + number, 0);
+    
+    // clickSpeed.textContent = (sum / l).toFixed(2)
+} 
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,19 +3,8 @@ let deadMole = document.getElementById("dead");
 
 let misses = document.getElementById("lost")
 
-// getHole = index => document.getElementById(`hole${index}`)
+const holeBox = Array.from(document.getElementsByClassName("hole"))
 
-const h1 = document.getElementById(`hole1`)
-const h2 = document.getElementById(`hole2`)
-const h3 = document.getElementById(`hole3`)
-const h4 = document.getElementById(`hole4`)
-const h5 = document.getElementById(`hole5`)
-const h6 = document.getElementById(`hole6`)
-const h7 = document.getElementById(`hole7`)
-const h8 = document.getElementById(`hole8`)
-const h9 = document.getElementById(`hole9`)
-
-let holeBox = [h1, h2, h3, h4, h5, h6, h7, h8, h9]
 
 // Декларативный подход через forEach
 holeBox.forEach(element => element.onclick = () => {
@@ -24,8 +13,20 @@ holeBox.forEach(element => element.onclick = () => {
     } else {
         misses.textContent = Number(misses.textContent) + 1;
     }
+
+    if (Number(misses.textContent) > 4) {
+        alert ("Вы проиграли!")
+        deadMole.textContent = 0; 
+        misses.textContent = 0;
     }
-)
+    
+    if(Number(deadMole.textContent) >= 10) {
+        alert ("Победа!")
+        deadMole.textContent = 0; 
+        misses.textContent = 0;
+    }
+
+})
 
 // Императивный подход через цикл for
 // for (let i = 0; i < 10; i++) {
